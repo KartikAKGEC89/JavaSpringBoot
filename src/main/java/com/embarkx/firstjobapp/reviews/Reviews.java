89dbd543afd2cf5ca6c7e11e247214f5b611cea6
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-08-17 17:34:45 +0530

    Add review file code this tell us about what we store in database

--- src/main/java/com/embarkx/firstjobapp/reviews/Reviews.java
+++ src/main/java/com/embarkx/firstjobapp/reviews/Reviews.java
@@ -1,4 +1,58 @@
 package com.embarkx.firstjobapp.reviews;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Reviews {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tittle;
+    private String description;
+    private double rating;
+
+    public Reviews() {
+    }
+
+//    public Reviews(Long id, String tittle, String description, double rating) {
+//        this.id = id;
+//        this.tittle = tittle;
+//        this.description = description;
+//        this.rating = rating;
+//    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
